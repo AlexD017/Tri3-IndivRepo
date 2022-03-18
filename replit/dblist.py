e205424cc2b2c3ba4813f49099718764e1b462d2
AlexD017 <89167210+AlexD017@users.noreply.github.com>
2022-03-18 11:16:14 -0700

    hack 1 -InfoDB lists

---
+++ replit/dblist.py
@@ -0,0 +1,25 @@
+InfoDb = []
+
+InfoDb.append({  
+               "CompanyName": "Toyota",  
+               "CEO": "Akio Toyoda",  
+               "Recent_Models":["2022 Toyota Corolla Cross", "Toyota bZ4X Concept", "2022 Toyota GR 86", "2022 Toyota GR Supra A91-CF Edition", "2022 Toyota Tacoma TRD Pro"]  
+              })  
+
+InfoDb.append({  
+               "CompanyName": "Ford",  
+               "CEO": "Jim Farley",  
+               "Recent_Models":["2022 Ford Mustang", "2022 Ford Bronco", "2022 Ford Ranger", "2022 Ford F-150", "2022 Ford Explorer"] 
+              })
+
+InfoDb.append({  
+               "CompanyName": "BMW",  
+               "CEO": "Oliver Zipse",  
+               "Recent_Models":["2022 BMW 2 Series Gran Coupe", "2021 BMW 2-Series", "2022 BMW 3-Series", "2022 BMW X1", "2022 BMW X6"]              
+              })
+
+InfoDb.append({  
+               "CompanyName": "Volkswagen",  
+               "CEO": "Herbert Diess",  
+               "Recent_Models":["Volkswagen Golf 8 GTI", "Volkswagen Tiguan Facelift", "Volkswagen Kombi T6", "Volkswagen Crafter Update", "2022 Volkswagen ID.4"] 
+              })
