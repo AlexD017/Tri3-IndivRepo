f6caf00331fc6f82e2128e07e0962a1bd8c87c89
AlexD017 <89167210+AlexD017@users.noreply.github.com>
2022-03-18 13:47:23 -0700

    Add hack 2 -InfoDB loop

--- replit/dbhack.py
+++ replit/dbhack.py
@@ -23,3 +23,41 @@ InfoDb.append({
                "CEO": "Herbert Diess",  
                "Recent_Models":["Volkswagen Golf 8 GTI", "Volkswagen Tiguan Facelift", "Volkswagen Kombi T6", "Volkswagen Crafter Update", "2022 Volkswagen ID.4"] 
               })
+
+def print_data(n):
+    print("CompanyName", InfoDb[n]["CompanyName"], "\nCeo", InfoDb[n]["Ceo"])  # using comma puts space between values
+    print("\t", "Recent Models: ", end="")  # \t is a tab indent, end="" make sure no return occurs
+    print(", ".join(InfoDb[n]["Recent_Models"]))  # join allows printing a string list with separator
+    print()
+
+
+def tester():
+    print("For loop")
+    for_loop()
+    print("While loop")
+    while_loop(0)  # requires initial index to start while
+    print("Recursive loop")
+    recursive_loop(0)  # requires initial index to start recursion
+
+def for_loop():
+    for n in range(len(InfoDb)):
+        print_data(n)
+        
+def while_loop(n):
+    while n < len(InfoDb):
+        print_data(n)
+        n += 1
+    return
+  
+def recursive_loop(n):
+    if n < len(InfoDb):
+        print_data(n)
+        recursive_loop(n + 1)
+    return # exit condition
+
+# Factorial of a number using recursion
+def recur_factorial(n):
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return n * recur_factorial(n-1)
