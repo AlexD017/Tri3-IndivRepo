07024d7850ca8fc709b70a453265cecc6b6e7fca
AlexD017 <89167210+AlexD017@users.noreply.github.com>
2022-03-20 17:20:32 -0700

    Update dbhack.py

--- replit/dbhack.py
+++ replit/dbhack.py
@@ -31,7 +31,28 @@ def print_data(n):
     print()
 
 
-def tester():
+def for_loop():
+    for n in range(len(InfoDb)):
+        print_data(n)
+
+
+# while loop contains an initial n and an index incrementing statement (n += 1)
+def while_loop(n):
+    while n < len(InfoDb):
+        print_data(n)
+        n += 1
+    return
+
+
+# recursion simulates loop incrementing on each call (n + 1) until exit condition is met
+def recursive_loop(n):
+    if n < len(InfoDb):
+        print_data(n)
+        recursive_loop(n + 1)
+    return
+
+
+def cars():
     print("For loop")
     for_loop()
     print("While loop")
@@ -39,28 +60,9 @@ def tester():
     print("Recursive loop")
     recursive_loop(0)  # requires initial index to start recursion
 
-def for_loop():
-    for n in range(len(InfoDb)):
-        print_data(n)
-        
-def while_loop(n):
-    while n < len(InfoDb):
-        print_data(n)
-        n += 1
-    return
-  
-def recursive_loop(n):
-    if n < len(InfoDb):
-        print_data(n)
-        recursive_loop(n + 1)
-    return # exit condition
 
-# Factorial of a number using recursion
-def recur_factorial(n):
-    if n == 1 or n == 0:
-        return 1
-    else:
-        return n * recur_factorial(n-1)
-      
+
+# this is test driver or code that plays when executed directly,
+# versus import which will not run these statements
 if __name__ == "__main__":
-    tester()
+    cars()
