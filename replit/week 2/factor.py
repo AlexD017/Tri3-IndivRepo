bc93db2d82d5c157c297bf0f94c7a27a3293e2fa
AlexDo <alexkwangmin@gmail.com>
2022-03-28 04:18:57 -0700

    Change from GCD to factor

---
+++ replit/week 2/factor.py
@@ -0,0 +1,15 @@
+def factors():
+    num = int(input("Enter any number: "))
+    findFactors(num)
+
+
+def findFactors(number):
+    print("Factors of the given number {0} is:".format(number))
+
+    for value in range(1, number + 1):
+        if number % value == 0:
+            print("{0}".format(value), end=" ")
+    print()
+
+
+factors()
